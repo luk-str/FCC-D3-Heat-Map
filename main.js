5fec07e2a236ca011ff0bd56a89209caabb4e77c
luk-str <lstrociak@gmail.com>
2020-08-04 12:14:42 +0200

    Define Scales + Add Axes

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
+// Define Sizes
+const margin = { top: 30, right: 50, bottom: 50, left: 100 };
+const w = 950;
+const h = 600;
+
+// Add Main Chart svg
+const svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
+
+// Load Data
 fetch(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
 )
   .then((response) => response.json())
   .then(function (data) {
-    // Define sizes
-    const margin = { top: 30, right: 50, bottom: 30, left: 60 };
-    const w = 1000 - margin.left - margin.right;
-    const h = 600 - margin.top - margin.bottom;
-
-    // Separate data elements into separate variables
+    // Separate Data Elements into Separate Variables
     const baseTemperature = data.baseTemperature;
     const monthlyData = data.monthlyVariance;
 
-    // Convert time data to usable date format
+    // Insert Base Temperature From Data into Description
+    document.getElementById(
+      "description"
+    ).innerText = `Base temperature: ${baseTemperature}℃`;
+
+    // Convert Time Data to Usable Date Format
     const parseYear = d3.timeParse("%Y");
     const parseMonth = d3.timeParse("%m");
 
@@ -21,10 +34,30 @@ fetch(
       d.month = parseMonth(d.month);
     });
 
-    // Add main chart svg
-    const svg = d3
-      .select(".chart")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+    // Define Scales
+    const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(monthlyData, (d) => d.year))
+      .range([margin.left, w - margin.right]);
+
+    const yScale = d3
+      .scaleBand()
+      .domain(monthlyData.map((d) => d.month))
+      .range([margin.top, h - margin.bottom]);
+
+    // Append X Axis
+    svg
+      .append("g")
+      .call(d3.axisBottom(xScale))
+      .attr("id", "x-axis")
+      .attr("class", "axis-bottom")
+      .attr("transform", `translate(0, ${h - margin.bottom})`);
+
+    // Append Y Axis
+    svg
+      .append("g")
+      .call(d3.axisLeft(yScale).tickFormat(d3.timeFormat("%B")))
+      .attr("id", "y-axis")
+      .attr("class", "axis-left")
+      .attr("transform", `translate(${margin.left}, 0)`);
   });
